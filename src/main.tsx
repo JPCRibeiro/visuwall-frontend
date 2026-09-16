e9e81db391282b32e587b37617a64f937a6310e1
JPCRibeiro <jopedroribeiro9@gmail.com>
2026-09-16 16:39:59 -0300

    chore: setup project typography and font variables

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,30 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import { App } from "./App.tsx";
+import App from "./App.tsx";
+import Home from "./pages/Home.tsx";
+import Wallpapers from "./pages/Wallpapers.tsx";
+import Favorites from "./pages/(protected)/Favorites.tsx";
+import Upload from "./pages/(protected)/Upload.tsx";
+import Register from "./pages/(auth)/Register.tsx";
+import Login from "./pages/(auth)/Login.tsx";
+import "@fontsource/fascinate";
+import '@fontsource/geist/400.css';
+import '@fontsource/geist/600.css';
+import '@fontsource/geist/700.css';
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: App,
+    children: [
+      { index: true, Component: Home },
+      { path: "wallpapers", Component: Wallpapers },
+      { path: "favoritos", Component: Favorites },
+      { path: "upload", Component: Upload },
+      { path: "register", Component: Register },
+      { path: "login", Component: Login },
+    ],
   },
 ]);
 
