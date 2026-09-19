4818f48d2941f23d560a2b3afef50e6a6c6b345d
JPCRibeiro <jopedroribeiro9@gmail.com>
2026-09-19 20:25:31 -0300

    fix: remove redundant padding from PageTitle component

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -8,7 +8,7 @@ type PageTitleProps = {
 
 export default function PageTitle({ Icon, title, description }: PageTitleProps) {
   return (
-    <div className="bg-[radial-gradient(500px_80px_at_0px_top,rgba(96,240,209,0.3),transparent)] py-3.5 px-7 pl-7 pt-3.5 pr-5">
+    <div className="bg-[radial-gradient(500px_80px_at_0px_top,rgba(96,240,209,0.3),transparent)] py-3.5 pl-7 pr-5">
       <h3 className="text-white text-[30px] font-bold [text-shadow:1px_2px_4px_#000] flex items-center gap-3 mb-2">
         <Icon className="drop-shadow-[0_4px_2px_#141414]" size={40} color="#03e3b8"/>
         {title}
